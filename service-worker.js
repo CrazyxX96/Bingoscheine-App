b0664c67e9cc3be165c8f6016da485028ac2c082
CrazyxX96 <schnioffsky-t@t-online.de>
2025-09-28 01:28:18 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+// Cache-first PWA shell
+const CACHE_NAME='bingo-pwa-v5';
+const ASSETS=[
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/icon-1024.png'
+];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME&&caches.delete(k))))) });
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
